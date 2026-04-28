f3ae6ff1f6c8c05a030a446ac71b0aaa3d4ad46c
Danny <danny.koshik@gmail.com>
2026-04-29 02:02:04 +0300

    fix checkstyle EndpointHitDto

--- stats-service/dto/src/main/java/ru/practicum/stats/dto/EndpointHitDto.java
+++ stats-service/dto/src/main/java/ru/practicum/stats/dto/EndpointHitDto.java
@@ -1,14 +1,8 @@
 package ru.practicum.stats.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-
 import java.time.LocalDateTime;
 
-public record EndpointHitDto(
-    Long id,
-    String app,
-    String uri,
-    String ip,
-    @JsonFormat(pattern = StatsConstants.DATE_TIME_PATTERN) LocalDateTime timestamp
-) {
+public record EndpointHitDto(Long id, String app, String uri, String ip,
+                             @JsonFormat(pattern = StatsConstants.DATE_TIME_PATTERN) LocalDateTime timestamp) {
 }
