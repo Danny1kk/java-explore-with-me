b0c89b9b06347d1771e6a3702f9ed2be14dc9f38
Danny <danny.koshik@gmail.com>
2026-04-28 23:22:58 +0300

    add package stats

--- stats-service/dto/src/main/java/ru/practicum/stats/dto/ViewStatsDto.java
+++ stats-service/dto/src/main/java/ru/practicum/stats/dto/ViewStatsDto.java
@@ -1,19 +1,4 @@
 package ru.practicum.stats.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class ViewStatsDto {
-
-    private String app;
-
-    private String uri;
-
-    private Long hits;
+public class ViewStatsDto(String app, String uri, Long hits) {
 }
