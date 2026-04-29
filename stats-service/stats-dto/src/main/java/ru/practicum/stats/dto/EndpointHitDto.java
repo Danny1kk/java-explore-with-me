72641746465461784d824f049b065035786d84a2
Danny <danny.koshik@gmail.com>
2026-04-30 00:39:43 +0300

    feat: move stats-server url to properties using @Value

--- stats-service/stats-dto/src/main/java/ru/practicum/stats/dto/EndpointHitDto.java
+++ stats-service/stats-dto/src/main/java/ru/practicum/stats/dto/EndpointHitDto.java
@@ -1,8 +1,27 @@
 package ru.practicum.stats.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
 import java.time.LocalDateTime;
 
-public record EndpointHitDto(Long id, String app, String uri, String ip,
-                             @JsonFormat(pattern = StatsConstants.DATE_TIME_PATTERN) LocalDateTime timestamp) {
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EndpointHitDto {
+
+    @NotBlank
+    private String app;
+
+    @NotBlank
+    private String uri;
+
+    @NotBlank
+    private String ip;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
 }
