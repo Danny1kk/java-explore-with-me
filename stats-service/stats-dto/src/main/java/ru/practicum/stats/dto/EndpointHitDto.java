a88d3e621d13dce4a80341baa8242e4100951716
Danny <danny.koshik@gmail.com>
2026-04-30 01:17:00 +0300

    fix: fix EndpointHitDto

--- stats-service/stats-dto/src/main/java/ru/practicum/stats/dto/EndpointHitDto.java
+++ stats-service/stats-dto/src/main/java/ru/practicum/stats/dto/EndpointHitDto.java
@@ -2,26 +2,13 @@ package ru.practicum.stats.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import javax.validation.constraints.NotBlank;
-import lombok.*;
-
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class EndpointHitDto {
+public record EndpointHitDto(
 
-    @NotBlank
-    private String app;
+    @NotBlank String app,
+    @NotBlank String uri,
+    @NotBlank String ip,
 
-    @NotBlank
-    private String uri;
-
-    @NotBlank
-    private String ip;
-
-    @JsonFormat(pattern = StatsConstants.DATE_TIME_PATTERN)
-    private LocalDateTime timestamp;
-}
+    @JsonFormat(pattern = StatsConstants.DATE_TIME_PATTERN) LocalDateTime timestamp
+) {}
