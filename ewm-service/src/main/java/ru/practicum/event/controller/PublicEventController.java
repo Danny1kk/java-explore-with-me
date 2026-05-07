20185288f8b90088eace23a5c3edc03866f2747e
Dmitrii Motorin <dmitrii-motorin@mail.ru>
2026-05-07 22:02:06 +0300

    StatsConnection

--- ewm-service/src/main/java/ru/practicum/event/controller/PublicEventController.java
+++ ewm-service/src/main/java/ru/practicum/event/controller/PublicEventController.java
@@ -18,24 +18,35 @@ public class PublicEventController {
     private final EventService eventService;
 
     @GetMapping
-    public List<EventShortDto> search(@RequestParam(required = false) String text,
-                                      @RequestParam(required = false) List<Long> categories,
-                                      @RequestParam(required = false) Boolean paid,
-                                      @RequestParam(required = false) LocalDateTime rangeStart,
-                                      @RequestParam(required = false) LocalDateTime rangeEnd,
-                                      @RequestParam(defaultValue = "false") Boolean onlyAvailable,
-                                      @RequestParam(required = false) String sort,
-                                      @RequestParam(defaultValue = "0") int from,
-                                      @RequestParam(defaultValue = "10") int size,
-                                      HttpServletRequest request) {
-        // Здесь позже будет сохранение хитов через StatsClient
-        return eventService.searchPublic(text, categories, paid, rangeStart, rangeEnd,
+    public List<EventShortDto> search(
+            @RequestParam(required = false) String text,
+            @RequestParam(required = false) List<Long> categories,
+            @RequestParam(required = false) Boolean paid,
+            @RequestParam(required = false) LocalDateTime rangeStart,
+            @RequestParam(required = false) LocalDateTime rangeEnd,
+            @RequestParam(defaultValue = "false") Boolean onlyAvailable,
+            @RequestParam(required = false) String sort,
+            @RequestParam(defaultValue = "0") int from,
+            @RequestParam(defaultValue = "10") int size,
+            HttpServletRequest request) {
+
+        List<EventShortDto> events = eventService.searchPublic(
+                text, categories, paid, rangeStart, rangeEnd,
                 onlyAvailable, sort, from, size);
+
+        // Отправка статистики просмотров списка
+        eventService.saveHitForEvents(events, request.getRemoteAddr());
+
+        return events;
     }
 
     @GetMapping("/{id}")
     public EventFullDto get(@PathVariable Long id, HttpServletRequest request) {
-        // Здесь позже будет сохранение хитов через StatsClient
-        return eventService.getPublic(id);
+        EventFullDto event = eventService.getPublic(id);
+
+        // Отправка статистики просмотра одного события
+        eventService.saveHit(id, request.getRemoteAddr());
+
+        return event;
     }
 }
